807fc9e6ef819269aa5f3202605fe40bdda52b8d
BroTarek <metarek257@gmail.com>
2026-04-30 16:39:56 +0300

    nav logo

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -65,8 +65,11 @@ export default function Navbar() {
                 <div className="flex justify-between h-16 items-center">
                     {/* Logo */}
                     <Link href="/" className="flex items-center">
-                        <span className="text-2xl font-bold tracking-tight text-primary-text font-sans">TheYoKo</span>
-                        <span className="text-kaizen-red text-2xl font-bold leading-none">.</span>
+                        <span className="text-2xl font-bold tracking-tight text-primary-text font-sans">
+                        <img src="https://theyoko.com/wp-content/uploads/2021/07/The-YoKo-BLK-NO-BG1.png" alt=""
+                        style={{width:"73.44px",height:"55px"}} />
+                        </span>
+                        {/* <span className="text-kaizen-red text-2xl font-bold leading-none">.</span> */}
                     </Link>
 
                     {/* Desktop Navigation - Hidden Sidebar Icon (as requested) */}
